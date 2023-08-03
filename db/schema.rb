49bc4f4921928c93cf7ad1505e09d6a856db2bb4
kawasakimaki <kchurip.08@gmail.com>
2023-08-03 11:07:22 +0000

    [Finish]2-5

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,9 @@ ActiveRecord::Schema.define(version: 2023_08_03_084250) do
   create_table "lists", force: :cascade do |t|
     t.string "title"
     t.string "body"
+    t.string "author"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
